dcd1e4d564cae39b1a79a594812b4be99c89fe8c
unknown <kalted1997@gmail.com>
2019-03-05 22:21:17 +0200

    Updates

---
+++ precache-manifest.bab86382fc8a0808f676e75c0026ed89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fede0bd58b04f0e3706164e3bae9674",
+    "url": "/neuron/static/media/Logo.7fede0bd.png"
+  },
+  {
+    "revision": "0ae01d9884ebae73bd45e13726fce59e",
+    "url": "/neuron/static/media/Background.0ae01d98.png"
+  },
+  {
+    "revision": "d491b413a5382c10b6fc",
+    "url": "/neuron/static/js/runtime~main.d491b413.js"
+  },
+  {
+    "revision": "0f42ff84f757ee52b1ab",
+    "url": "/neuron/static/js/main.0f42ff84.chunk.js"
+  },
+  {
+    "revision": "3b9d478091b70b3f7aa3",
+    "url": "/neuron/static/js/2.3b9d4780.chunk.js"
+  },
+  {
+    "revision": "0f42ff84f757ee52b1ab",
+    "url": "/neuron/static/css/main.5e858f18.chunk.css"
+  },
+  {
+    "revision": "3b9d478091b70b3f7aa3",
+    "url": "/neuron/static/css/2.1e5b6a41.chunk.css"
+  },
+  {
+    "revision": "dd0bb60a08f13fc1830e8dc11a714758",
+    "url": "/neuron/index.html"
+  }
+];
