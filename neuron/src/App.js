266353e739f5339e2cd12c52d5a55146275a90e7
unknown <kalted1997@gmail.com>
2019-03-07 15:25:42 +0200

    Added switch button

--- neuron/src/App.js
+++ neuron/src/App.js
@@ -4,11 +4,25 @@ import './App.css';
 import Table from './Table.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      class: "Button"
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    const state = this.state.class;
+    (state === "Button" ? this.setState({class: "Button-clicked"}) : this.setState({class: "Button"}));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} alt="" className="App-logo"/>
+          <button onClick={this.handleClick} className={this.state.class}>Manual <span>Auto</span></button>
           <div>
             <Table />
           </div>
