7df126c8ee558c355eaad6c17f09bd5d7fbfca15
Deivydas Kinderis <kalted1997@gmail.com>
2019-04-23 19:30:39 +0300

    Updates

---
+++ precache-manifest.9b62382954f548290bb77610747ea762.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fede0bd58b04f0e3706164e3bae9674",
+    "url": "/neuron/static/media/Logo.7fede0bd.png"
+  },
+  {
+    "revision": "0ae01d9884ebae73bd45e13726fce59e",
+    "url": "/neuron/static/media/Background.0ae01d98.png"
+  },
+  {
+    "revision": "d491b413a5382c10b6fc",
+    "url": "/neuron/static/js/runtime~main.d491b413.js"
+  },
+  {
+    "revision": "f7e7eef5d3253b96eb07",
+    "url": "/neuron/static/js/main.f7e7eef5.chunk.js"
+  },
+  {
+    "revision": "e06e97f1d0c6899e46f2",
+    "url": "/neuron/static/js/2.e06e97f1.chunk.js"
+  },
+  {
+    "revision": "f7e7eef5d3253b96eb07",
+    "url": "/neuron/static/css/main.917faf20.chunk.css"
+  },
+  {
+    "revision": "e06e97f1d0c6899e46f2",
+    "url": "/neuron/static/css/2.1e5b6a41.chunk.css"
+  },
+  {
+    "revision": "21615586d45817f960c9cbefd91368e8",
+    "url": "/neuron/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/neuron/precache-manifest.bdd721ab3b3b739c356d109d8a94f080.js"
+  "/neuron/precache-manifest.9b62382954f548290bb77610747ea762.js"
 );
 
 workbox.clientsClaim();
